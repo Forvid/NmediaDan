5ba2c53f43460e24123d5788d2b16a53a99e5638
Forvid <vitodanlanc@gmail.com>
2024-12-18 23:34:19 +0600

    исправлено форматирование чисел

--- app/src/main/java/ru/netology/nmedia/dto/Utils.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Utils.kt
@@ -3,7 +3,11 @@ package ru.netology.nmedia.dto
 fun formatCount(count: Int): String {
     return when {
         count >= 1_000_000 -> String.format("%.1fM", count / 1_000_000.0).replace(",", ".")
-        count >= 10_000 -> "${count / 1_000}K"
+        count >= 10_000 -> {
+
+            val result = (count / 1_000.0)
+            "${Math.floor(result * 10) / 10}K"
+        }
         count >= 1_000 -> String.format("%.1fK", count / 1_000.0).replace(",", ".")
         else -> count.toString()
     }
