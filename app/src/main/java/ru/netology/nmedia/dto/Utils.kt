c08ffceda082dc164b0ab51b96c2655b3d3ec4a8
Forvid <vitodanlanc@gmail.com>
2024-12-18 21:31:26 +0600

    изменён метод formatCount для корректной обработки

--- app/src/main/java/ru/netology/nmedia/dto/Utils.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Utils.kt
@@ -2,9 +2,9 @@ package ru.netology.nmedia.dto
 
 fun formatCount(count: Int): String {
     return when {
-        count >= 1_000_000 -> String.format("%.1fM", count / 1_000_000.0)
+        count >= 1_000_000 -> String.format("%.1fM", count / 1_000_000.0).replace(",", ".")
         count >= 10_000 -> "${count / 1_000}K"
-        count >= 1_000 -> "${count / 1_000}K"
+        count >= 1_000 -> String.format("%.1fK", count / 1_000.0).replace(",", ".")
         else -> count.toString()
     }
 }
